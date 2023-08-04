d29c3e230d02bdc7513c7e22df3a603e78ebc9cd
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-04 22:13:45 +0600

    initial commit

---
+++ CarRentalProgram/CarRentalProgram/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CarRentalProgram
+//
+//  Created by Farhan Niloy on 4/8/23.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
